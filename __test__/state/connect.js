34577ac6e26072e5562740a4ac4059018763bdc9
Scott Corgan <scottcorgan@gmail.com>
2015-08-06 16:03:56 -0700

    testing connect

---
+++ __test__/state/connect.js
@@ -0,0 +1,37 @@
+import {namespace} from 'tessed';
+import React from 'react';
+
+import connect from 'lw-state/connect';
+import createStore from 'lw-state/create-store';
+
+let test = namespace('connect');
+
+test.only('store with selector', () => {
+
+  let initialState = {
+    foo: 'bar',
+    test: 'ing'
+  };
+
+  let myReducer = function (state, action) {
+
+    switch (action.type) {
+      default:
+        return state;
+    };
+  };
+  let store = createStore(myReducer, initialState);
+
+  class TestComponent extends React.Component {
+
+    render () {
+
+      return <div>Testing</div>
+    }
+  }
+
+  // connectStore or selectState ????
+  let ConnectedComponent = connect(store)(state => state)(TestComponent);
+});
+
+test('selector only');

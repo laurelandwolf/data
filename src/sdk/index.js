1476314765bfbdba5982a28061b83e0acb07ed3c
Garron Sanchez <garron@laurelandwolf.com>
2017-04-25 10:31:17 -0700

    allow headers to be a function

--- src/sdk/index.js
+++ src/sdk/index.js
@@ -4,23 +4,42 @@ import {merge} from 'lodash'
 import api from './api'
 import {DEFAULT_HEADERS} from './headers'
 
+function configGlobalSpec (globalSpec) {
+
+  let headers
+  if (typeof globalSpec.headers === 'function') {
+    headers = {...globalSpec.headers()}
+  }
+  else {
+    headers = globalSpec.headers
+  }
+
+  return {
+    ...globalSpec,
+    ...{headers}
+  }
+}
+
 export default function sdk (globalSpec = {}) {
 
   let defaultSpec = {
     origin: '',
     headers: DEFAULT_HEADERS
   }
-  let options = merge({}, defaultSpec, globalSpec)
+
+  let options = merge({}, defaultSpec, configGlobalSpec(globalSpec))
+
   let stream = Observable.create()
 
   function apiFactory (instanceSpec = {}) {
 
-  	return api(merge({}, options, instanceSpec, {
-  		getStream: () => stream
-  	}))
+    options = merge({}, defaultSpec, configGlobalSpec(globalSpec))
+    return api(merge({}, options, instanceSpec, {
+        getStream: () => stream
+    }))
   }
-
   apiFactory.createStream = callback => stream = callback(options)
+
 
   return apiFactory
 }
